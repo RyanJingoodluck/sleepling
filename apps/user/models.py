234d458e1182088ba6477e1a8b5fd67d8502a046
Ryan Jin <461569429@qq.com>
2019-07-18 23:58:18 +0800

    add user fields

--- apps/user/models.py
+++ apps/user/models.py
@@ -8,6 +8,11 @@ class User(AbstractUser):
     """
     用户表
     """
+    USER_GENDER_CHOICES = (
+        (0, '女'),
+        (1, '男'),
+    )
+
     phone = models.CharField(
         max_length=32,
         db_index=True,
@@ -16,3 +21,10 @@ class User(AbstractUser):
         verbose_name="手机号码",
         help_text="手机号码"
     )
+    sex = models.SmallIntegerField(choices=USER_GENDER_CHOICES, default=1, verbose_name="性别")
+    avatar = models.CharField(max_length=50, default="", null=True, blank=True, verbose_name="头像")
+    openid = models.CharField(max_length=64, db_index=True, verbose_name='openid')
+
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
